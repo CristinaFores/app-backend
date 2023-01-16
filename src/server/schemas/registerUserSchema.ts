29ccee570775ad742e7cab6d41fcca080ae7babc
Cristina <cristinaforescampos1992@gmail.com>
2023-01-16 23:47:56 +0100

    Create registerUserSchema

---
+++ src/server/schemas/registerUserSchema.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Joi } from "express-validation";
+
+const registerUserSchema = {
+  body: Joi.object({
+    username: Joi.string().min(2).required().messages({
+      "string.empty": `Your username cannot be empty.`,
+      "string.min": `Your username should have a minimum length of {#limit} characters.`,
+      "any.required": `Username is a required field`,
+    }),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(5).required().messages({
+      "string.empty": `Your password cannot be empty.`,
+      "string.min": `Your password should have a minimum length of {#limit} characters.`,
+      "any.required": `Password is a required field`,
+    }),
+  }),
+};
+
+export default registerUserSchema;
