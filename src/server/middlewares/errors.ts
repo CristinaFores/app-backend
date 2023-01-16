0e0f577d5408d5025c9c34c286c8aea8c25bef2e
Cristina <cristinaforescampos1992@gmail.com>
2023-01-17 00:27:04 +0100

    Create middlware unknownEndpoint and generalError

---
+++ src/server/middlewares/errors.ts
@@ -0,0 +1,43 @@
+import "../../loadEnviroment.js";
+import type { NextFunction, Request, Response } from "express";
+import CustomError from "../../CustomError/CustomError.js";
+import { ValidationError } from "express-validation";
+import { httpStatusCodes } from "../../utils/statusCodes.js";
+
+const {
+  clientErrors: { notFoundError },
+  serverError: { errorGeneral },
+} = httpStatusCodes;
+
+export const unknownEndpoint = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { path } = req;
+
+  next(
+    new CustomError(
+      `Unknown Endpoint: ${path}`,
+      notFoundError,
+      "Unknown endpoint"
+    )
+  );
+};
+
+export const generalError = (
+  error: CustomError | ValidationError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line no-unused-vars
+  next: NextFunction
+) => {
+  if (error instanceof ValidationError) {
+    return res.status(400).json(error);
+  }
+
+  const statusCode = error.statusCode ?? errorGeneral;
+  const publicMessage = error.publicMesage || "There is a error general";
+
+  res.status(statusCode).json({ error: publicMessage });
+};
