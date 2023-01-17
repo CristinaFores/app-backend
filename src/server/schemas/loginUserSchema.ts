fb49009c33f008e3f703f0fbe0d4f836b5fdcd14
Cristina <cristinaforescampos1992@gmail.com>
2023-01-17 17:23:47 +0100

    Add loginUserSchema

---
+++ src/server/schemas/loginUserSchema.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Joi } from "express-validation";
+
+const loginUserSchema = {
+  body: Joi.object({
+    username: Joi.string().min(2).required().messages({
+      "string.min": "Username must have 2 characters minimum",
+      "string.empty": "Username is required",
+    }),
+    password: Joi.string().min(5).required().messages({
+      "string.min": "Password should have 5 characters minimum",
+      "string.empty": "Password is required",
+    }),
+  }),
+};
+
+export default loginUserSchema;
