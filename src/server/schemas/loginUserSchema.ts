03aec0992028a112f9c066ef3f146f140153d3b5
Cristina <cristinaforescampos1992@gmail.com>
2023-01-17 18:10:23 +0100

    Add email in loginUserSchema

--- src/server/schemas/loginUserSchema.ts
+++ src/server/schemas/loginUserSchema.ts
@@ -7,6 +7,7 @@ const loginUserSchema = {
       "string.min": "Username must have 2 characters minimum",
       "string.empty": "Username is required",
     }),
+    email: Joi.string().email(),
     password: Joi.string().min(5).required().messages({
       "string.min": "Password should have 5 characters minimum",
       "string.empty": "Password is required",
