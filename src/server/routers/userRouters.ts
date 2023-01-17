1965507075fbb3ee75503009ee84c7535a402598
Cristina <cristinaforescampos1992@gmail.com>
2023-01-17 17:25:37 +0100

    Add router enpoint login

--- src/server/routers/userRouters.ts
+++ src/server/routers/userRouters.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import { validate } from "express-validation";
-import { register } from "../controllers/users/users.js";
+import { login, register } from "../controllers/users/users.js";
+import loginUserSchema from "../schemas/loginUserSchema.js";
 import registerUserSchema from "../schemas/registerUserSchema.js";
 
 const userRouters = express.Router();
+
 userRouters.post(
   "/register",
   validate(registerUserSchema, {}, { abortEarly: false }),
   register
+);
+
+userRouters.post(
+  "/login",
+  validate(loginUserSchema, {}, { abortEarly: false }),
+  login
 );
 
 export default userRouters;
