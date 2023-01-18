791d7fe25a73cf08de9deca43d00224919664bbc
Cristina <cristinaforescampos1992@gmail.com>
2023-01-18 16:43:02 +0100

    Add app cors

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,16 @@
 import "../loadEnviroment.js";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { generalError, unknownEndpoint } from "./middlewares/errors.js";
 import userRouters from "./routers/userRouters.js";
+import corsOptions from "./cors/corsOptions.js";
 
 const app = express();
+
+app.disable("x-powered-by");
+app.use(cors(corsOptions));
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/", userRouters);
