6914cbb30fe75d41897ee4228b3fc282a68d2cb5
Cristina <cristinaforescampos1992@gmail.com>
2023-01-20 22:22:46 +0100

    Add getNotes in routers

--- src/server/routers/notesRouters,ts/notesRouters.ts
+++ src/server/routers/notesRouters,ts/notesRouters.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { newNote, updateNote } from "../../controllers/notes/notes.js";
+import { getNote, newNote, updateNote } from "../../controllers/notes/notes.js";
 
 const notesRouters = express.Router();
 
 notesRouters.post("/note", newNote);
-notesRouters.put("/note", updateNote);
+notesRouters.patch("/note/:id", updateNote);
+notesRouters.get("/notes", getNote);
 
 export default notesRouters;
