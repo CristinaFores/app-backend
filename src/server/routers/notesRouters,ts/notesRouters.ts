23800057ef2e2435073e634eeec687b2481a18d4
Cristina <cristinaforescampos1992@gmail.com>
2023-01-21 13:19:47 +0100

    Change routers notes PUT and change for enpoint /note/:id

--- src/server/routers/notesRouters,ts/notesRouters.ts
+++ src/server/routers/notesRouters,ts/notesRouters.ts
@@ -4,7 +4,7 @@ import { getNote, newNote, updateNote } from "../../controllers/notes/notes.js";
 const notesRouters = express.Router();
 
 notesRouters.post("/note", newNote);
-notesRouters.patch("/note/:id", updateNote);
+notesRouters.put("/note/:id", updateNote);
 notesRouters.get("/notes", getNote);
 
 export default notesRouters;
