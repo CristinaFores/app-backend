9b317a7141f68fd24cca5ad2e043a8c38994022e
Cristina <cristinaforescampos1992@gmail.com>
2023-05-11 19:07:30 +0200

    Add url env

--- src/server/cors/allowedOrigins.ts
+++ src/server/cors/allowedOrigins.ts
@@ -1,8 +1,8 @@
 const allowedOrigins = [
   "http://localhost:4000",
-  "http://localhost:3001",
+  "http://localhost:3000",
   "https://blackboards-notes.netlify.app",
-  "https://blackboards-notes.netlify.app/",
+  "https://app-backend-production.up.railway.app",
 ];
 
 export default allowedOrigins;
